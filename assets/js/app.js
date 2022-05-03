e32acee6e38d7928e70155a0ab72748bc1deffcd
ChauveauAdrien <adrienpro.chauveau@gmail.com>
2022-05-03 11:43:37 +0200

    css

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,14 +2,16 @@ let app = new Vue({
     el: "#myApp",
 
     data: {
-        info: null
+        info: null,
+        
     },
     methods: {
+        
         
     },
     mounted () {
         axios
-          .get('https://api.openweathermap.org/data/2.5/weather?lat=43&lon=5&appid=d31f06ee007a3b205abd05ca07748597')
+          .get('https://api.openweathermap.org/data/2.5/onecall?lat=49.4929&lon=0.1134&exclude=hourly,minutely&units=metric&appid=0b714303b59b32e758cc43e063f1b1aa')
           .then(response => (this.info = response))
       }
 })
