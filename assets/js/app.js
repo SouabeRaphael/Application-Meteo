73a71351b264f1887f7f3d1f95576fa8396ce5c1
ChauveauAdrien <adrienpro.chauveau@gmail.com>
2022-05-03 10:16:54 +0200

    test axios

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,21 @@ let app = new Vue({
     el: "#myApp",
 
     data: {
-
+        info: null
     },
     methods: {
-
-    }
+        
+    },
+    mounted () {
+        axios
+          .get('https://api.openweathermap.org/data/2.5/weather?lat=43&lon=5&appid=d31f06ee007a3b205abd05ca07748597')
+          .then(response => (this.info = response))
+      }
 })
+
+
+// const instance = axios.create({
+//     baseURL: 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=d31f06ee007a3b205abd05ca07748597',
+//   });
+
+
